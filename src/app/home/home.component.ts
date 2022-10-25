76adb519eeaf6d7751485264de623602f5e5306d
unknown <thyagoalmeidaprof@gmail.com>
2022-10-25 15:05:57 -0300

    HOME

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  imagemHome = "https://cdn.pixabay.com/photo/2020/04/20/14/47/car-5068532_960_720.png";
   constructor() { }
 
   ngOnInit(): void {
