061095b6f691b78f7da5d7d825f38cda86e42cbc
unknown <thyagoalmeidaprof@gmail.com>
2022-10-25 15:12:28 -0300

    json

--- src/app/foto/foto.component.ts
+++ src/app/foto/foto.component.ts
@@ -7,38 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotoComponent implements OnInit {
   title = "Minhas Fotos";
-  fotos = [
-    {
-      img: "https://cdn.pixabay.com/photo/2017/07/05/19/32/oldtimer-2475746__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Vermelho"
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2018/01/28/16/31/auto-3114102__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Verde"
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2017/07/05/19/32/oldtimer-2475748__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Rosa"
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2018/01/28/16/37/auto-3114120__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Cinza"
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2014/08/04/17/03/chrysler-409795__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Garfite"
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2018/05/07/19/58/car-3381549__340.jpg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit odio eget ultrices porttitor.",
-      titulo: "Carro Branco"
-    }
-  ]
+  fotos = [];
   constructor() {
 
    }
